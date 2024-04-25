f23ce835d53abc14772f6d85bf523575e80f5fb4
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2024-04-25 09:48:54 +0530

    added the condition for baseline image

--- terraform/image_gallery/main.tf
+++ terraform/image_gallery/main.tf
@@ -9,11 +9,13 @@ resource "azurerm_shared_image_gallery" "this" {
 }
 
 resource "random_id" "baseline" {
+  count       = var.image_definition_baseline == "cloudops-shared-baseline-windows2022" ? 1 : 0
   prefix      = "2022-datacenter-"
   byte_length = 8
 }
 
 resource "azurerm_shared_image" "baseline" {
+  count                    = var.image_definition_baseline == "cloudops-shared-baseline-windows2022" ? 1 : 0
   name                     = var.image_definition_baseline
   gallery_name             = azurerm_shared_image_gallery.this.name
   resource_group_name      = data.azurerm_resource_group.this.name
--- terraform/image_gallery/outputs.tf
+++ terraform/image_gallery/outputs.tf
@@ -1,4 +1,4 @@
-output "resource_group" {
+output "image_gallery_resource_group" {
   value = data.azurerm_resource_group.this.name
 }
 
@@ -11,9 +11,21 @@ output "shared_gallery" {
 }
 
 output "shared_image_baseline" {
-  value = azurerm_shared_image.baseline.name
+  value = [azurerm_shared_image.baseline.*.name]
 }
 
 output "shared_image_generalized" {
   value = azurerm_shared_image.generalized.name
+}
+
+output "subnet" {
+  value = azurerm_subnet.this.id
+}
+
+output "key_vault" {
+  value = azurerm_key_vault.this.id
+}
+
+output "secret_name" {
+  value = azurerm_key_vault_secret.password.name
 }
